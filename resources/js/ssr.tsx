7a8776f67474ed9dfca1f3d0c44114f53ba2f552
itachi880 <142436626+itachi880@users.noreply.github.com>
2026-02-08 18:52:09 +0100

    fix init app

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -12,8 +12,8 @@ createServer((page) =>
         title: (title) => (title ? `${title} - ${appName}` : appName),
         resolve: (name) =>
             resolvePageComponent(
-                `./Pages/${name}.tsx`,
-                import.meta.glob('./Pages/**/*.tsx') as any,
+                `./pages/${name}.tsx`,
+                import.meta.glob('./pages/**/*.tsx') as any,
             ),
         setup: ({ App, props }) => {
             return <App {...props} />;
